f52eaf828c8c593d302d6e8b7743e22641647284
Maron hawa <38923401+maronhawa1@users.noreply.github.com>
2025-11-24 13:47:44 +0200

    Modify MongoDB URI for connection settings
    Updated MongoDB URI to include retryWrites and w parameters.

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 # הכנס כאן את ה-URI שלך ממונגו אטלס או מקומי
-MONGO_URI = "mongodb+srv://maronhawa:maron12345@barbershop-db.a7xwgix.mongodb.net/?appName=barbershop-db"
+MONGO_URI = "mongodb+srv://maronhawa:Maron12345@barbershop-db.a7xwgix.mongodb.net/?retryWrites=true&w=majority&appName=barbershop-db"
 
 client = MongoClient(MONGO_URI)
 db = client["barbershop_db"]
